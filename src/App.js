86c2ba334f0f612f2f6ab84cdfa4c810d7b4ed0d
luisromo <lromo@iteso.mx>
2020-08-25 00:07:43 -0500

    🔫

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+      <h1 className="title">LuisFerGRomo</h1>
+      {/* <image src=""></image> */}
+      <div className="socialMedia-container">
+        <a className="socialMedia" href="https://fb.me/LuisFerGRomoMx">
+          <FontAwesomeIcon icon={faFacebook} />
         </a>
-      </header>
+        <a className="socialMedia" href="https://instagram.com/luisfergromo/">
+          <FontAwesomeIcon icon={faInstagram} />
+        </a>
+        <a className="socialMedia" href="https://linkedin.com/in/LuisFerGRomo/">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+      </div>
+      <a
+        aria-hidden="true"
+        className="btn btn-link"
+        href="https://luisfergromo.web.app/"
+        rel="noopener"
+      >
+        Personal Web Page
+      </a>
     </div>
   );
 }
