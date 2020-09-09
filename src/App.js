0a77662b026701c9c97b012416ef5fc5cfd49977
luisromo <lromo@iteso.mx>
2020-09-08 22:49:24 -0500

    Latest

--- src/App.js
+++ src/App.js
@@ -3,11 +3,19 @@ import "./App.css";
 import Footer from "./components/Footer";
 import SocialMedia from "./components/Social";
 import Links from "./components/Links";
+import github from "./github.svg";
 
 function App() {
+  var name = "LuisFerGRomo";
   return (
     <div className="App">
-      <h1 className="title">LuisFerGRomo</h1>
+      <div className="header">
+        <a href="https://github.com/luisfergromo/1link-access">
+          <img className="github-clone" src={github} alt="" />
+        </a>
+      </div>
+
+      <h1 className="title">{name}</h1>
       <SocialMedia />
       <br />
       <Links />
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,13 +3,15 @@ import React, { Component } from "react";
 class Footer extends Component {
   render() {
     return (
-      <p className="footer">
-        Made with{" "}
-        <span aria-hidden="true" role="img">
-          ♥️
-        </span>{" "}
-        by Luis Romo
-      </p>
+      <div className="footer">
+        <p>
+          Made with{" "}
+          <span aria-hidden="true" role="img">
+            ♥️
+          </span>{" "}
+          by Luis Romo
+        </p>
+      </div>
     );
   }
 }
